c2d190ac8b71ae6faaaa38e0be7a2587bddae140
Александр <cahei29@rambler.ru>
2018-08-21 00:05:17 +0300

    - немного дизайна - создал страницы

---
+++ src/views/newMessage.vue
@@ -0,0 +1,9 @@
+<template>
+    <form>
+        <h2>Новое сообщение</h2>
+        <input placeholder="Получатель" class="input">
+        <input placeholder="Тема" class="input">
+        <input placeholder="Сообщение" class="input">
+        <button type="submit" class="btn">Отправить</button>
+    </form>
+</template>
