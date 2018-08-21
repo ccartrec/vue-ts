33c20b342e58d18bfab6a17b2e4d1a158c646d4f
Александр <cahei29@rambler.ru>
2018-08-22 00:20:46 +0300

    - немного дизайна - рефакторинг страниц

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/new" class="btn">Новое сообщение</router-link>
-      <router-link to="/">Входящие</router-link>
-      <router-link to="/about">Исходящие</router-link>
+      <router-link to="/new" class="nav-link new">Новое сообщение</router-link>
+      <router-link to="/" class="nav-link incoming">Входящие</router-link>
+      <router-link to="/outgoing" class="nav-link outgoing">Исходящие</router-link>
     </div>
     <div class="app-body">
       <router-view/>
